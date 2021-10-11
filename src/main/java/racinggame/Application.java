7ffccd8b6c68d8fe88325f71959d4198bb083903
InmookJeong <jeonginmook@gmail.com>
2021-10-11 20:45:15 +0900

    feat(Application): 자동차 이름 입력받기 기능 추가
    - (기능1) 자동차 이름을 입력받을 수 있는 기능 추가

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,7 +1,31 @@
 package racinggame;
 
+import nextstep.utils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 자동차 경주 게임 구현
+        String inputCarNames = inputCarNames();
+        System.out.println("사용자가 입력한 자동차 이름 : " + inputCarNames);
     }
+
+    // TODO : 기능 1. 자동차 이름 입력
+    public static String inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+    
+    // TODO : 기능 2. 자동차 이름이 5자 이하인지 체크
+
+    // TODO : 기능 3. 사용자 시도횟수 입력
+    
+    // TODO : 기능 4. 시도횟수가 숫자인지 체크
+    
+    // TODO : 기능 5. 임의의 숫자를 가져와 전진할 것인지 체크
+    
+    // TODO : 기능 6. 자동차 별 전진한 수 출력
+    
+    // TODO : 기능 7. 자동차 전진횟수 중 시도횟수와 일치하는 값이 있는지 확인
+    
+    // TODO : 기능 8. 일치하는 값이 있으면 시도횟수에 도달한 자동차의 명단 출력 후 게임 종료
 }
